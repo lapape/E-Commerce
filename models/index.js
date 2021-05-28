ab0aec96fdebce67d050aad30786e06547eb008d
lapape <lapape07@gmail.com>
2021-05-27 19:43:23 -0600

    added category routes

--- models/index.js
+++ models/index.js
@@ -21,6 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
+  as: "product_tags",
 });
 // Tags belongToMany Products (through ProductTag)
 
@@ -29,6 +30,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
+  as: "tagged_products",
 });
 
 module.exports = {
